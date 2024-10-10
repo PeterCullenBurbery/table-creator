40112f39c53148d930a3d1fb7991d3e6929e52da
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2024-10-10 15:58:34 -0400

    I added more functionality to starter page.
    I want the person to have an area to enter ideas for their starter table. then there should be another idea for the final idea. then there should be an area to load their final idea into what the app will process. the ideas should be a larger text box because they might have a lot of ideas. if they click load final idea, it will apply some transformations to their final idea. It will suggest a new name where spaces are replaced with _ like dog cat bird will become dog_cat_bird. It will also show them an all uppercase DOG_CAT_BIRD and all lowercase dog_cat_bird. It will also show them the length in characters and length in bytes. It will also show them a list of words like {"dog","cat","bird"}. It will also show how many words so 3 for dog_cat_bird, 2 for play_mode, 1 for difficulty.
    
    Help me make this with starter table page.

--- WpfApp1/WpfApp1/StarterTablePage.xaml.cs
+++ WpfApp1/WpfApp1/StarterTablePage.xaml.cs
@@ -30,5 +30,38 @@ namespace WpfApp1
             // Navigate back to the HomePage
             NavigationService.Navigate(new HomePage());
         }
+
+        private void LoadFinalIdea_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the final idea text
+            string finalIdea = FinalIdeaTextBox.Text.Trim();
+
+            // If the final idea is empty, show an error message
+            if (string.IsNullOrWhiteSpace(finalIdea))
+            {
+                MessageBox.Show("Please enter a final idea before loading.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Transformations
+            string suggestedName = finalIdea.Replace(" ", "_");
+            string upperCaseName = suggestedName.ToUpper();
+            string lowerCaseName = suggestedName.ToLower();
+            int characterLength = suggestedName.Length;
+            int byteLength = Encoding.UTF8.GetByteCount(suggestedName);
+
+            // Split the suggested name by underscores to get the list of words
+            string[] words = suggestedName.Split('_');
+            int wordCount = words.Length;
+
+            // Display the results in the appropriate text boxes
+            SuggestedNameTextBox.Text = suggestedName;
+            UpperCaseNameTextBox.Text = upperCaseName;
+            LowerCaseNameTextBox.Text = lowerCaseName;
+            CharacterLengthTextBox.Text = characterLength.ToString();
+            ByteLengthTextBox.Text = byteLength.ToString();
+            WordsListTextBox.Text = "{" + string.Join(", ", words.Select(w => $"\"{w}\"")) + "}";
+            WordCountTextBox.Text = wordCount.ToString();
+        }
     }
 }
